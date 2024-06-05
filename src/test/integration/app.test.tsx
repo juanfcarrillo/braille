ca2d0d5575f69f91cacd81ac969740b9b0a421d9
juanfcarrillo <juanfi444@outlook.com>
2024-06-04 19:44:19 -0500

    test: add test for App.tsx

---
+++ src/test/integration/app.test.tsx
@@ -0,0 +1,22 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import { describe, expect, it } from 'vitest'
+import App from '../../App'
+
+// Given
+describe("[Integration] Braille translator", () => {
+    // When
+    describe('user types in spanish alphabet', () => {
+        // Then
+        it('translates the input to braille', async () => {
+            render(<App/>)
+
+            fireEvent.change(screen.getByTestId('alpha-input'), { target: {value: 'hola'} })
+
+            expect(
+                screen.getByTestId('braille-output')
+            ).toHaveValue(
+                '⠓⠕⠇⠁'
+            )
+        } )
+    })
+})
