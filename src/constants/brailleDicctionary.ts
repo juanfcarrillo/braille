1d1022d1683a4c152fab571ff98e0bdfbd874af0
JhonMeza7 <jhon.meza@epn.edu.ec>
2024-07-08 22:28:10 -0500

    doc: comment the code

--- src/constants/brailleDicctionary.ts
+++ src/constants/brailleDicctionary.ts
@@ -1,78 +1,86 @@
+// Objeto que mapea letras del alfabeto español a su representación en braille
 export const alphabet = {
-    'a':  '⠁',
-    'á':  '⠷',
-    'b':  '⠃',
-    'c':  '⠉',
-    'd':  '⠙',
-    'e':  '⠑',
-    'é':  '⠮',
-    'f':  '⠋',
-    'g':  '⠛',
-    'h':  '⠓',
-    'i':  '⠊',
-    'í':  '⠌',   
-    'j':  '⠚',
-    'k':  '⠅',
-    'l':  '⠇',
-    'm':  '⠍',
-    'n':  '⠝',
-    'ñ':  '⠻',
-    'o':  '⠕',
-    'ó':  '⠬',
-    'p':  '⠏',
-    'q':  '⠟',
-    'r':  '⠗',
-    's':  '⠎',
-    't':  '⠞',
-    'u':  '⠥',
-    'ú':  '⠾',
-    'v':  '⠧',
-    'w':  '⠺',
-    'x':  '⠭',
-    'y':  '⠽',
-    'z':  '⠵',
+  'a': '⠁',
+  'á': '⠷',
+  'b': '⠃',
+  'c': '⠉',
+  'd': '⠙',
+  'e': '⠑',
+  'é': '⠮',
+  'f': '⠋',
+  'g': '⠛',
+  'h': '⠓',
+  'i': '⠊',
+  'í': '⠌',
+  'j': '⠚',
+  'k': '⠅',
+  'l': '⠇',
+  'm': '⠍',
+  'n': '⠝',
+  'ñ': '⠻',
+  'o': '⠕',
+  'ó': '⠬',
+  'p': '⠏',
+  'q': '⠟',
+  'r': '⠗',
+  's': '⠎',
+  't': '⠞',
+  'u': '⠥',
+  'ú': '⠾',
+  'v': '⠧',
+  'w': '⠺',
+  'x': '⠭',
+  'y': '⠽',
+  'z': '⠵',
 };
 
-export const UPPER_CASE_PREFIX = '⠅'
-export const NUMERAL_PREFIX = '⠼'
+// Prefijo para indicar letras mayúsculas en braille
+export const UPPER_CASE_PREFIX = '⠅';
 
+// Prefijo para indicar que el siguiente carácter es un numeral en braille
+export const NUMERAL_PREFIX = '⠼';
+
+// Objeto que mapea números del 0 al 9 a su representación en braille
 export const numbers = {
-    '1':  '⠁',
-    '2':  '⠃',
-    '3':  '⠉',
-    '4':  '⠙',
-    '5':  '⠑',
-    '6':  '⠋',
-    '7':  '⠛',
-    '8':  '⠓',
-    '9':  '⠊',
-    '0':  '⠚',
-}
+  '1': '⠁',
+  '2': '⠃',
+  '3': '⠉',
+  '4': '⠙',
+  '5': '⠑',
+  '6': '⠋',
+  '7': '⠛',
+  '8': '⠓',
+  '9': '⠊',
+  '0': '⠚',
+};
 
+// Objeto que mapea signos de puntuación y otros caracteres especiales a su representación en braille
 export const punctuation = {
-    '.': '⠄',
-    ',':  '⠂',
-    '\\?': '⠢',
-    '\\¿': '⠢',
-    ';':  '⠆',
-    '!':  '⠖',
-    '¡':  '⠖',
-    '<':  '⠦',
-    '>':  '⠴',
-    '\\ ': '⠶',
-    '-':  '⠤',
-    "'":  '⠄',
-}
+  '.': '⠄',
+  ',': '⠂',
+  '\\?': '⠢',
+  '\\¿': '⠢',
+  ';': '⠆',
+  '!': '⠖',
+  '¡': '⠖',
+  '<': '⠦',
+  '>': '⠴',
+  '\\ ': '⠶',  // Espacio
+  '-': '⠤',
+  "'": '⠄',   // Apóstrofo
+};
 
-// Reverse mappings for Braille to text
+// Mapeo inverso para convertir braille de vuelta a texto
 export const brailleToAlphabet = Object.fromEntries(
-    Object.entries(alphabet).map(([key, value]) => [value, key])
-  );
-  
-  export const brailleToNumbers = Object.fromEntries(
-    Object.entries(numbers).map(([key, value]) => [value, key])
-  );
-  
-  export const brailleToPunctuation = Object.fromEntries(
-    Object.entries(punctuation).map(([key, value]) => [value, key])
-  );
+  Object.entries(alphabet).map(([key, value]) => [value, key])
+);
+
+// Mapeo inverso para números
+export const brailleToNumbers = Object.fromEntries(
+  Object.entries(numbers).map(([key, value]) => [value, key])
+);
+
+// Mapeo inverso para signos de puntuación
+export const brailleToPunctuation = Object.fromEntries(
+  Object.entries(punctuation).map(([key, value]) => [value, key])
+);
